63ad3d8f96e5a6f25e51fd839ac82037cc7b321f
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-08 19:56:18 +0200

    Image comparer demo.

---
+++ ImageComparisonRanker/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ImageComparisonRanker
+{
+    using ImageMagick;
+
+    class Program
+    {
+        static void Main(string[] args) {
+            var image1Path = @".\cat1.png";
+            var image2Path = @".\cat1.png";
+            var image3Path = @".\catDiff.png";
+
+            MagickImage image1 = new MagickImage(image1Path);
+            image1.Compose = CompositeOperator.Difference;
+            image1.ColorFuzz = new Percentage(5);
+            MagickImage image2 = new MagickImage(image2Path);
+            image2.Compose = CompositeOperator.Difference;
+
+            MagickImage diffImage = new MagickImage();
+
+            double diff = image1.Compare(image2, ErrorMetric.Absolute, diffImage);
+
+            diffImage.Write(image3Path);
+        }
+    }
+}
