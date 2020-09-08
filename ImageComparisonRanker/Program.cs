f1adb2f9f3eef3009cbebb8e2f4c3f7f8821d53a
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-08 20:03:15 +0200

    Get all pngs on current folder. Compare reference image to each of them.

--- ImageComparisonRanker/Program.cs
+++ ImageComparisonRanker/Program.cs
@@ -1,25 +1,22 @@
 ﻿namespace ImageComparisonRanker
 {
     using ImageMagick;
+    using System.IO;
 
     class Program
     {
         static void Main(string[] args) {
-            var image1Path = @".\cat1.png";
-            var image2Path = @".\cat1.png";
-            var image3Path = @".\catDiff.png";
+            DirectoryInfo d = new DirectoryInfo(@".\");
+            FileInfo[] Files = d.GetFiles("*.png");
 
-            MagickImage image1 = new MagickImage(image1Path);
-            image1.Compose = CompositeOperator.Difference;
-            image1.ColorFuzz = new Percentage(5);
-            MagickImage image2 = new MagickImage(image2Path);
-            image2.Compose = CompositeOperator.Difference;
+            MagickImage referenceImage = new MagickImage(args[0]);
+            referenceImage.Compose = CompositeOperator.Difference;
+            referenceImage.ColorFuzz = new Percentage(5);
 
-            MagickImage diffImage = new MagickImage();
-
-            double diff = image1.Compare(image2, ErrorMetric.Absolute, diffImage);
-
-            diffImage.Write(image3Path);
+            foreach (FileInfo file in Files) {
+                MagickImage image = new MagickImage(file.FullName);
+                double diff = referenceImage.Compare(image, ErrorMetric.Absolute);
+            }
         }
     }
 }
